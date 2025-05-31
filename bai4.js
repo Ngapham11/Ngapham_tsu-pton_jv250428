ffd72854d0d268c237730b2c83723ebcf17be3c4
Ngapham11 <phuchang7577@gmail.com>
2025-05-31 11:48:30 +0900

    session10-bai4.html

---
+++ bai4.js
@@ -0,0 +1,3 @@
+let firstNumber = Number(prompt("Moi ban nhap so thu nhat:"));
+let seconNumber = Number(prompt("Moi ban nhap so thu hai:"));
+alert("Tong hai so la:" + (firstNumber + seconNumber));
