e08b188c696af478d12377e2beb4e4b318d5090a
Ngapham11 <phuchang7577@gmail.com>
2025-05-31 12:01:09 +0900

    ss10-bai6.html

---
+++ bai6.js
@@ -0,0 +1,4 @@
+let math = Number(prompt("Xin moi nhap diem toan:"));
+let physics = Number(prompt("Xin moi nhap diem Vat ly:"));
+let chemistry = Number(prompt("Xin moi nhap diem hoa hoc:"));
+console.log("Diem trung binh la:" + (math + physics + chemistry) / 3);
