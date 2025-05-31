37b113e4b5e98e91c7321bc45b2dd0865b5aafa4
Ngapham11 <phuchang7577@gmail.com>
2025-05-31 11:48:03 +0900

    session10-bai3.html

---
+++ bai3.js
@@ -0,0 +1,4 @@
+let width = Number(prompt("nhap chieu dai:"));
+let height = Number(prompt("nhap chieu cao:"));
+let area = Number(width * height);
+console.log("Dien tich hinh chu nhat la:" + area);
