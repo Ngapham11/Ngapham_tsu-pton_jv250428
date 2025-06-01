01101119520d3068215e14be36f33a36b3da6cfe
Ngapham11 <phuchang7577@gmail.com>
2025-06-01 17:30:45 +0900

    ss11-bai3.html

---
+++ ss11-bai3.js
@@ -0,0 +1,33 @@
+let nhapSo = prompt(`Hay nhap so tu mot den chin.`);
+switch (nhapSo) {
+  case `1`:
+    console.log(`So mot`);
+    break;
+  case `2`:
+    console.log(`So hai`);
+    break;
+  case `3`:
+    console.log(`So ba`);
+    break;
+  case `4`:
+    console.log(`So bon`);
+    break;
+  case `5`:
+    console.log(`So nam`);
+    break;
+  case `6`:
+    console.log(`So sau`);
+    break;
+  case `7`:
+    console.log(`So bay`);
+    break;
+  case `8`:
+    console.log(`So tam`);
+    break;
+  case `9`:
+    console.log(`So chin`);
+    break;
+  default:
+    console.log(`khong xac dinh`);
+    break;
+}
