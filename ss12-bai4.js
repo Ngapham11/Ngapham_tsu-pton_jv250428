d61165b6a2617bdad70b67fd4bda9ae69993933b
Ngapham11 <phuchang7577@gmail.com>
2025-06-04 00:54:48 +0900

    ss12-bai4.html

---
+++ ss12-bai4.js
@@ -0,0 +1,21 @@
+// //bai1
+// for (let i = 1; i <= 100; i = i + 1) {
+//   console.log(i);
+
+//   if (i === 99) {
+//     console.log(`Da hoan thanh`);
+//     break;
+//   }
+// }
+
+// //bai2
+// let a = Number(prompt(`Moi ban nhap nhiet do hien tai:`));
+// if (a >= 20 && a <= 100) {
+//   console.log(`Nhiet do phu hop.`);
+// } else if (a < 20) {
+//   console.log(`Ban hay tang nhiet do.`);
+// } else if (a > 100) {
+//   console.log(`Ban hay giam nhiet do.`);
+// } else {
+//   console.log(`Khong xac dinh duoc nhiet do.`);
+// }
