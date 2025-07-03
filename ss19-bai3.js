c1be24d99b794d8cb01ff0114932be554623dc25
Ngapham11 <phuchang7577@gmail.com>
2025-07-03 23:33:47 +0900

    ss19-bai3

---
+++ ss19-bai3.js
@@ -0,0 +1,25 @@
+//so sanh thong tin nhap vao va thong tin co trong he thong
+//B1: lay thong tin tren he thong
+let users = JSON.parse(localStorage.getItem(`users`)) || [];
+console.log(users);
+//lay thong tin nguoi dung nhap vao
+let form = document.getElementById(`form`);
+form.addEventListener(`submit`, function (e) {
+  e.preventDefault();
+
+  let email = document.getElementById(`email`).value;
+
+  let password = document.getElementById(`password`).value;
+
+  //so sanh thong tin nguoi dung va thong tin luu tren he thong
+  let result = users.some(
+    (element) => (element.email == email) & (element.password == password)
+  );
+
+  if (!result) {
+    alert(`Dang nhap khong thanh cong`);
+  } else {
+    alert(`Dang nhap thanh cong`);
+    window.location.href = `http://127.0.0.1:5500/project-root/pages/Dashboard.html`;
+  }
+});
