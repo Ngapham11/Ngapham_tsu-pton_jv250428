998903dac5c4d4347b113b5471cbb0fad4872047
Ngapham11 <phuchang7577@gmail.com>
2025-07-12 15:55:54 +0900

    ss2-bai4

---
+++ ColorBox.jsx
@@ -0,0 +1,12 @@
+import "../main.css";
+export default function ColorBox() {
+  return (
+    <>
+      <div className="container">
+        <div className="box box1">Box</div>
+        <div className="box box2">Box</div>
+        <div className="box box3">Box</div>
+      </div>
+    </>
+  );
+}
