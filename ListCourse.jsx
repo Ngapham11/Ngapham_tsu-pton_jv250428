eda16bc288fd9e2fb818a89c90deecd13b3c15fa
Ngapham11 <phuchang7577@gmail.com>
2025-07-12 15:53:15 +0900

    ss2-bai1

---
+++ ListCourse.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function ListCourse() {
+  return (
+    <>
+      <div>
+        <h2>Danh sách khóa học</h2>
+        <div>
+          <ol>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>ReactJS</li>
+          </ol>
+        </div>
+      </div>
+    </>
+  );
+}
