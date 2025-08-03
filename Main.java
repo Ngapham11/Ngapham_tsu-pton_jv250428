ddd8c965e80fd9edde300a570461ef3936ee192c
Ngapham11 <phuchang7577@gmail.com>
2025-08-03 23:02:16 +0900

    SS11-bai1

--- Main.java
+++ Main.java
@@ -1,62 +1,18 @@
-package SS10.bai3;
+package SS11.bai1;
 
-import javax.xml.namespace.QName;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
-    public static LinkedList<Users>usersLinkedList=new LinkedList<>();
     public static void main(String[] args) {
-
-        Scanner scanner=new Scanner(System.in);
-        do {try {
-            System.out.println("\n****MENU QUAN LY NGUOI DUNG****\n" +
-                    "1.Them nguoi dung\n" +
-                    "2.Xoa nguoi dung\n" +
-                    "3.Hien thi danh sach nguoi dung\n" +
-                    "4.Thoat\n");
-            System.out.println("Lua chon cua ban la:");
-            int choice=Integer.parseInt(scanner.nextLine());
-            switch (choice){
-                case 1://them nguoi dung
-                    System.out.println("Them nguoi dung");
-                    String name=InputData.inputUserName(scanner);
-                    String email=InputData.inputEmail(scanner);
-                    String telephon=InputData.inputTelephon(scanner);
-                    Users newUsers=new Users(name,email,telephon);
-                    usersLinkedList.add(newUsers);
-                    break;
-                case 2:
-                    System.out.println("Xoa nguoi dung");
-                    System.out.println("Nhap ten nguoi dung muon xoa :");
-                    String deleteName=InputData.inputUserName(scanner);
-                    usersLinkedList.removeIf(users->users.getUserName().equals(deleteName));
-                    System.out.println("Da xoa nguoi dung "+deleteName);
-
-                    break;
-                case 3:
-                    System.out.println("Hien thi danh sach nguoi dung");
-                    if (usersLinkedList.isEmpty()){
-                        System.out.println("Danh sach rong");
-                    }else {
-                        for (Users users : usersLinkedList) {
-                            users.displayUserData();
-                        }
-                    }
-                    break;
-                case 4:
-                    System.exit(0);
-
-                    break;
-                default:
-                    System.err.println("Hay nhap 1-4");
-                    break;
+        List<Integer>numbers= Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        numbers.forEach(number->{
+            if (number%2==0){
+                System.out.println(number);
             }
-        }catch (Exception e){
-            System.out.println("Hay nhap 1-4");
-        }
-
-        }while (true);
+        });
+        int sum = numbers.stream().mapToInt(Integer::intValue).sum();
+        System.out.println("Tổng của tất cả các số trong danh sách: " + sum);
     }
-
 }
