36171ba9b91bec9b95456a3a6030bab794385f19
Ngapham11 <phuchang7577@gmail.com>
2025-08-26 01:27:34 +0900

    Pham Thi Nga

--- Main.java
+++ Main.java
@@ -1,80 +1,39 @@
-import ss16_bai1.dao.StudentDao;
-import ss16_bai1.dao.StudentDaoimpl;
-import ss16_bai1.entity.Student;
+package business;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         do {
-            System.out.println("Tạo chương trình Java với menu như sau:\n" +
-                    "1.Hiển thị danh sách sinh viên.\n" +
-                    "2.Thêm mới sinh viên.\n" +
-                    "3.Sửa sinh viên.\n" +
-                    "4.Xóa sinh viên.\n" +
-                    "5.Tìm kiếm sinh viên.\n" +
-                    "6.Thoát .");
-            System.out.println("Moi ban chon 1-6");
+            System.out.println("\n*****************WORKSHOP MANAGEMENT*****************\n" +
+                    "1. Danh sách workshop\n" +
+                    "2. Thêm mới workshop\n" +
+                    "3. Cập nhật workshop\n" +
+                    "4. Xóa workshop\n" +
+                    "5. Tìm kiếm workshop theo tên diễn giả\n" +
+                    "6. Sắp xếp workshop theo ngày thực hiện\n" +
+                    "7.Thoat\n");
+            System.out.println("Moi ban nhap tu 1-7");
             int choice=Integer.parseInt(scanner.nextLine());
             switch (choice){
-                case 1:displayData();
+                case 1:workShopBusiness.displayWorkShop();
                     break;
-                case 2:createStudent(scanner);
+                case 2:workShopBusiness.createWorkShop(scanner);
                     break;
-                case 3:updateStudent(scanner);
+                case 3:workShopBusiness.updateWorkShop(scanner);
                     break;
-                case 4:deleteStudent(scanner);
+                case 4:workShopBusiness.deleteWorkShop(scanner);
                     break;
-                case 5:finfById(scanner);
+                case 5:workShopBusiness.findByName(scanner);
                     break;
-                case 6:System.exit(0);
-                break;
+                case 6:workShopBusiness.sortWorkShopByDate();
+                    break;
+                case 7:System.exit(0);
+                    break;
                 default:
-                    System.out.println("Moi ban chon 1-6");
+                    System.out.println("Ban hay chon 1-7");
             }
         }while (true);
-    }
-    public static final StudentDao studentDao=new StudentDaoimpl();
-    public static void displayData(){
-       List<Student>students= studentDao.findAll();
-       students.forEach(System.out::println);
-    }
-    public static void createStudent(Scanner scanner){
-        Student student=new Student();
-        student.inputData(scanner);
-       if (studentDao.addStudent(student)){
-           System.out.println("Them thanh cong");
-       }else System.out.println("Them that bai");
-    }
-    public static void updateStudent(Scanner scanner){
-
-        System.out.println("Moi ban nhap studentId muon sua");
-        int id=Integer.parseInt(scanner.nextLine());
-      Student student =studentDao.findById(id);
-      if (student!=null){
-          student.inputData(scanner);
-          if (studentDao.updateStudent(student)){
-              System.out.println("Sua thanh cong");
-          }else System.out.println("Cap nhat that bai");
-      }else System.out.println("Khong tim thay");
-    }
-    public static void deleteStudent(Scanner scanner){
-        System.out.println("Moi ban nhap studentId muon sua");
-        int id=Integer.parseInt(scanner.nextLine());
-        Student student=studentDao.findById(id);
-        if (student!=null){
-           if (studentDao.deleteStudent(student)){
-               System.out.println("Xoa thanh cong");
-           }else System.out.println("Xoa that bai");;
-        }else System.out.println("Khong tim thay");
-    }public static void finfById(Scanner scanner){
-        System.out.println("Moi ban nhap vao id muon tim :");
-        int id=Integer.parseInt(scanner.nextLine());
-        Student student=studentDao.findById(id);
-        if (student==null){
-            System.out.println("Khong tim thay");
-        }else System.out.println(student);
     }
 }
