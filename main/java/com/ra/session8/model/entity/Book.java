d8869c6c1c4161bdc3107cdaa4dc7e92881f2afa
Ngapham11 <phuchang7577@gmail.com>
2025-09-24 22:21:04 +0900

    ss8_bai1

---
+++ main/java/com/ra/session8/model/entity/Book.java
@@ -0,0 +1,24 @@
+package com.ra.session8.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private Double price;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
---
+++ main/java/com/ra/session8/model/entity/Category.java
@@ -0,0 +1,21 @@
+package com.ra.session8.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+ private Long id;
+ private String cateName;
+ private String description;
+ private boolean status;
+}
