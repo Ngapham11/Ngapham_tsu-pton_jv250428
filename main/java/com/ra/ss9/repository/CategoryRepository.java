d8cdb686599078137708c57587f52731147cff37
Ngapham11 <phuchang7577@gmail.com>
2025-09-27 12:01:33 +0900

    ss9-bai1

---
+++ main/java/com/ra/ss9/repository/CategoryRepository.java
@@ -0,0 +1,23 @@
+package com.ra.ss9.repository;
+
+import com.ra.ss9.model.entity.Category;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CategoryRepository {
+    @Autowired
+    private SessionFactory sessionFactory;
+    public List<Category>findAll(){
+        Session session=sessionFactory.getCurrentSession();
+      return   session.createQuery("from Category ",Category.class).getResultList();
+    }
+    public Category findById(long id){
+        Session session=sessionFactory.getCurrentSession();
+        return session.get(Category.class,id);
+    }
+}
